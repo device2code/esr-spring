071cb3efbedf5962ba532a0b7c5de665a2788ca8
Andre Scherrer <andrescherrer@gmail.com>
2020-10-17 10:56:25 -0300

    Adiciona inclusao de recursos de restaurantes

--- src/main/java/br/com/device2code/algafood/api/controller/RestauranteController.java
+++ src/main/java/br/com/device2code/algafood/api/controller/RestauranteController.java
@@ -3,14 +3,20 @@ package br.com.device2code.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.device2code.algafood.domain.exception.EntidadeNaoEncontradaException;
 import br.com.device2code.algafood.domain.model.Restaurante;
 import br.com.device2code.algafood.domain.repository.RestauranteRepository;
+import br.com.device2code.algafood.domain.service.CadastroRestauranteService;
 
 @RestController
 @RequestMapping(value = "/restaurantes")
@@ -18,6 +24,9 @@ public class RestauranteController {
 	
 	@Autowired
 	private RestauranteRepository restauranteRespository;
+	
+	@Autowired
+	private CadastroRestauranteService cadastroRestaurante;
 	
 	@GetMapping
 	public List<Restaurante> listar() {
@@ -33,5 +42,20 @@ public class RestauranteController {
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<?> adicionar (@RequestBody Restaurante restaurante) {
+		try {
+			restaurante = cadastroRestaurante.salvar(restaurante);
+			
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(restaurante);
+			
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest()
+					.body(e.getMessage());
+		}
 	}
 }
